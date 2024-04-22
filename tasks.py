1cd6efa8a8bf206537270014c3620f0990483353
Nathan <sylljay@gmail.com>
2024-04-22 15:18:45 +0200

    Included workItems and improved script robustness

--- tasks.py
+++ tasks.py
@@ -7,20 +7,24 @@ from time import sleep  # Import sleep function for waiting
 from robocorp import browser
 import time
 import re
-from datetime import datetime
+import datetime
 import requests
 import shutil
 import uuid
 import pandas as pd
 from contextlib import suppress
+from RPA.Robocorp.WorkItems import WorkItems
+import json
+
 
 
 class NewsProcessor:
-    def __init__(self, browser,search_phrase, news_category):
+    def __init__(self, browser,search_phrase, news_category, months_range=1):
         self.browser = browser
         self.search_phrase = search_phrase  # Store search_phrase as an attribute
         self.news_category = news_category
         self.file_name = f"challenge_{uuid.uuid4()}.xlsx"
+        self.months_range = months_range
 
     # Function to process news data
     def process_news(self,browser, search_phrase: str, news_category: str):
@@ -52,6 +56,18 @@ class NewsProcessor:
 
     # Function to click on search bar and enter search phrase
     def click_and_enter_search(self,page, search_phrase: str):
+
+        
+        # Click on the consent button if it exists
+        CONSENT_SELECTOR = "body > div.fc-consent-root > div.fc-dialog-container > div.fc-dialog.fc-choice-dialog > div.fc-footer-buttons-container > div.fc-footer-buttons > button.fc-button.fc-cta-consent.fc-primary-button"
+        sleep(3)
+        consent_button = page.query_selector(CONSENT_SELECTOR)
+        if consent_button:
+            consent_button.click()
+            print("Consent button clicked.")
+        else:
+            print("Consent button not found.")
+
         # Click on the search bar
         page.click("#__nuxt > div > div > main > header > div.top.flex.justify-content-between.align-items-center.sm\:align-items-end > div.gothamist-header-right.align-items-center.gap-2 > div.search-button > button")
 
@@ -108,7 +124,7 @@ class NewsProcessor:
             
             # Get the absolute path to the "output\images" directory
             current_directory = os.getcwd()  # Get the current working directory
-            images_directory = os.path.join(current_directory, "output", "images")
+            images_directory = os.path.join(current_directory, "output")
             os.makedirs(images_directory, exist_ok=True)  # Create the directory if it doesn't exist
             
             # Construct the full path to save the image
@@ -144,20 +160,22 @@ class NewsProcessor:
         month_number = months.get(month)
         
         # Construct and return the datetime object
-        return str(year)+"_-_"+str(month_number)+"_-_"+str(day)
-        
+        return datetime.datetime(int(year), month_number, int(day))
+
     # Function to extract data and store in CSV file
     def extract_and_store_data(self, page):
         # Define the selector for the load more button
         load_more_button_selector = "#resultList > div.col > button > span.p-button-label"
 
-        self.delete_images()
-
         # Initialize an empty list to store data
         data = []
-        with suppress(AssertionError):
-            browser.wait_until_element_is_visible("//*[contains(text(), 'Consent']", 45)
-            browser.click_element("//*[contains(text(), 'Consent']")
+
+        # Get current date
+        current_date = datetime.datetime.now()
+
+        # Define the start date based on the months range
+        start_date = current_date - datetime.timedelta(days=30 * self.months_range)
+
         # Extract the number of articles from the page
         num_articles_selector = "#__nuxt > div > div > main > div:nth-child(3) > div > section:nth-child(1) > div > div > div.col > div > span > strong"
         num_articles_element = page.query_selector(num_articles_selector)
@@ -169,7 +187,6 @@ class NewsProcessor:
             title_selector = f"#resultList > div.col > div:nth-child({i}) > div > div.card-details > div.card-title > a > div"
             category_selector = f"#resultList > div.col > div:nth-child({i}) > div > div.card-details > div.card-title > span > a > div > span"
             description_selector = f"#resultList > div.col > div:nth-child({i}) > div > div.card-details > div.card-slot > p"
-            image_selector = f"#resultList > div.col > div:nth-child({i}) > div > div.card-image-link.card-image-wrapper > figure:nth-child(2) > div > div > a > div > img"
 
             # Extract information for the current news item
             title_element = page.query_selector(title_selector)
@@ -184,58 +201,82 @@ class NewsProcessor:
             description_element = page.query_selector(description_selector)
             description = description_element.inner_text() if description_element else "Unknown"
 
-            image_element = page.query_selector(image_selector)
-            image_filename = ""
-            if image_element:
-                # Get the image source URL
-                image_url = image_element.get_attribute("src")
-
-                # Download the image
-                image_filename = self.download_image(image_url, title)
-                print("Downloaded image filename:", image_filename)
-
             # Click on the title to navigate to the full article page
             title_element.click()
+            print("clicked title")
             sleep(5)  # Wait for the page to load
 
             # Extract author and date of publication from the article page
             author_element = page.query_selector("#__nuxt > div > div > main > div:nth-child(3) > section.top-section > div > div:nth-child(2) > div.col.overflow-hidden > div.block.xxl\:hidden.mb-5 > div > div.author.flex.one-author > div.flex.flex-column.gap-125 > div.v-byline > div > a")
             author = author_element.inner_text() if author_element else "Unknown"
-
+            sleep(3)           
+                            
             date_element = page.query_selector("#__nuxt > div > div > main > div:nth-child(3) > section.top-section > div > div:nth-child(2) > div.col.overflow-hidden > div.block.xxl\:hidden.mb-5 > div > div.author.flex.one-author > div.flex.flex-column.gap-125 > div.date-published > p:nth-child(1)")
+            date_element_alt = page.query_selector("#__nuxt > div > div > main > div:nth-child(3) > section.top-section > div > div:nth-child(2) > div.col.overflow-hidden > div.block.xxl\:hidden.mb-5 > div > div.author.flex.one-author > div.flex.flex-column.gap-125 > div.date-published > p")
+            date_element_alt2 = page.query_selector("#__nuxt > div > div > main > div:nth-child(3) > section.top-section > div > div:nth-child(2) > div.col.overflow-hidden > div.block.xxl\:hidden.mb-5 > div > div.author.flex.multiple-authors > div.flex.flex-column.gap-125 > div.date-published > p:nth-child(1)")                                       
+                                                   
+            sleep(2)                               
             date_published_string = date_element.inner_text() if date_element else "Unknown"
-
+            print(date_published_string,"_-_-_-_-","main",date_element)
+            if date_published_string=="Unknown":#in some instances date element doesn't exist
+                date_published_string = date_element_alt.inner_text() if date_element_alt else "Unknown"
+                print(date_published_string,"_-_-_-_-alt",date_element_alt)
+            if date_published_string=="Unknown":#in some instances date element doesn't exist
+                date_published_string = date_element_alt2.inner_text() if date_element_alt2 else "Unknown"
+                print(date_published_string,"_-_-_-_-alt",date_element_alt2)
             # Parse the date string into datetime object
-            date_published = str(self.parse_date_string(date_published_string) if date_published_string != "Unknown" else "Unknown")
-            date_published = date_published.replace(" ", " at ")
-            date_published = "'" + date_published
+            date_published = self.parse_date_string(date_published_string) if date_published_string != "Unknown" else "Unknown"
+            
+            # Check if the article falls within the specified date range
+            if date_published >= start_date:
+                print(date_published,"--",start_date)
+                # Print the title, author, and date of publication for debugging
+                print("Title:", title)
+                print("Author:", author)
+                print("Date of Publication:", date_published)
+                print("Description:", description)
+                IsInRange = True
+                
+                # Count occurrences of search phrase in title and description
+                search_phrase_title_count = self.count_search_phrase_in_title(title, self.search_phrase)
+                search_phrase_description_count = self.count_search_phrase_in_description(description, self.search_phrase)
 
-            # Print the title, author, and date of publication for debugging
-            print("Title:", title)
-            print("Author:", author)
-            print("Date of Publication:", date_published)
-            print("Description:", description)
+                # Check if title or description contains any amount of money
+                money_present = self.contains_money(title, description)
 
-            # Count occurrences of search phrase in title and description
-            search_phrase_title_count = self.count_search_phrase_in_title(title, self.search_phrase)
-            search_phrase_description_count = self.count_search_phrase_in_description(description, self.search_phrase)
+                # Print search phrase counts and money presence
+                print("Search phrase '{}' count in title: {}".format(self.search_phrase, search_phrase_title_count))
+                print("Search phrase '{}' count in description: {}".format(self.search_phrase, search_phrase_description_count))
+                print("Money present in title or description:", money_present)
 
-            # Check if title or description contains any amount of money
-            money_present = self.contains_money(title, description)
-
-            # Print search phrase counts and money presence
-            print("Search phrase '{}' count in title: {}".format(self.search_phrase, search_phrase_title_count))
-            print("Search phrase '{}' count in description: {}".format(self.search_phrase, search_phrase_description_count))
-            print("Money present in title or description:", money_present)
-
-            # Append the data to the list
-            print("Appending data for:", title, "Date of Publication:", date_published)
-            data.append([title, author, date_published, description, search_phrase_title_count,
-                        search_phrase_description_count, money_present, image_filename])
+                # Append the data to the list
+                print("Appending data for:", title, "Date of Publication:", date_published)
+   
+            else:
+                IsInRange = False
 
             # Navigate back to the main page
+            sleep(2)
             page.go_back()
-            sleep(7)  # Wait for the page to load again
+            sleep(5)  # Wait for the page to load again
+            #dwnloads image if it's within the date range
+            if IsInRange:
+                image_selector = f"#resultList > div.col > div:nth-child({i}) > div > div.card-image-link.card-image-wrapper > figure:nth-child(2) > div > div > a > div > img"
+                image_element = page.query_selector(image_selector)
+                image_filename = "None"
+                if image_element:
+                    # Get the image source URL
+                    image_url = image_element.get_attribute("src")
+
+                    # Download the image
+                    image_filename = self.download_image(image_url, title)
+                    print("Downloaded image filename:", image_filename)
+                    data.append([title, author, date_published, description, search_phrase_title_count,
+                            search_phrase_description_count, money_present, image_filename])   
+                else:
+                    print("Image selector not found")
+            
+
 
             # If it's not the last article, click on the load more button to load more articles
             if i < num_articles:
@@ -258,7 +299,6 @@ class NewsProcessor:
         df.to_csv(csv_file, index=False)
 
         print("Data saved to CSV file:", csv_file)
-
    
 
 # Define task to solve the RPA challenge
@@ -266,24 +306,39 @@ class NewsProcessor:
 def solve_challenge():
     """
     Solve the RPA challenge
-    
     """
+    # Initialize WorkItems library
+    items = WorkItems()
+    
+    # Attempt to get values from Robocloud work items
+    try:
+        search_phrase = items.get_work_item_variable("search_phrase")
+        news_category = items.get_work_item_variable("news_category")
+        months_range = int(items.get_work_item_variable("months_range"))
+    except RuntimeError:
+        # If work items are not found, load default values from workitems.json
+        with open("workitems.json", "r") as f:
+            workitems = json.load(f)
+            payload = workitems[0]["payload"]
+            search_phrase = payload.get("search_phrase", "Nigeria Africa")
+            news_category = payload.get("news_category", "NEWS")
+            months_range = int(payload.get("months_range", 1))
+
+    # Configure the browser
     browser.configure(
         browser_engine="chromium",
         screenshot="only-on-failure",
         headless=True,
     )
+    
     try:
-        search_phrase = "Nigeria Africa"
-        news_category = "NEWS"  # No category needed for Gothamist
-        
         # Create an instance of NewsProcessor
-        news_processor = NewsProcessor(browser, search_phrase, news_category)
+        news_processor = NewsProcessor(browser, search_phrase, news_category, months_range)
         
         # Process news data using NewsProcessor instance
-        news_processor.process_news(browser, search_phrase, news_category,)
+        news_processor.process_news(browser, search_phrase, news_category)
 
     finally:
-        # Place for teardown and cleanups
+        # Teardown and cleanup
         # Playwright handles browser closing
         print('Done')
